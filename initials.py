d404b9f4298e2c75e7ae8cc05e1749c46ac7d2e2
edmundoteng11-creator <edmundoteng11@gmail.com>
2026-06-01 07:47:48 +0000

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,32 @@
+#File: initials.py
+#Description: Print out my initials
+#Assignment: Number 1
+#
+#Name: Edmund Oteng Manka
+#STUDENT ID: 2425402961
+#Email: 2425402961@live.gctu.edu.gh
+#Grader: Augustus Buckman
+#
+#On my honour, I Edmund Oteng Manka, this programming assignment is my own work
+#and I have not provided this code to any other student
 
 def main():
-  pass #code goes here
+    # Print initials (small) and then in large block letters made of the initial letters and periods
 
+    print()
+    print("...EOM")
+    print()
+    print("...EEEEEEEEEEEE.........OOOOOOOOOO.........MM........MM.....")
+    print("...EE..................OO........OO........MMM......MMM.....")
+    print("...EE..................OO........OO........MMMM....MMMM.....")
+    print("...EE..................OO........OO........MM.MM..MM.MM.....")
+    print("...EEEEEEEEEE..........OO........OO........MM..MMMM..MM.....")
+    print("...EEEEEEEEEE..........OO........OO........MM..MMMM..MM.....")
+    print("...EE..................OO........OO........MM........MM.....")
+    print("...EE..................OO........OO........MM........MM.....")
+    print("...EE............**....OO........OO..**....MM........MM..**.")
+    print("...EEEEEEEEEEEE..**.....OOOOOOOOOO...**....MM........MM..**.")
+    print()
 
 
 main()
